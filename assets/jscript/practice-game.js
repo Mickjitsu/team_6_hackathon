73175edc38c6df0979b61d229cb79059714533a3
Dayana <dayana.nashkova@yahoo.com>
2024-09-22 17:54:26 +0000

    add keyboard controls to practice mode

--- assets/jscript/practice-game.js
+++ assets/jscript/practice-game.js
@@ -420,4 +420,41 @@ function selectDifficulty() {
   });
 }
 
+const keyMap = {
+  q: 'key01',
+  w: 'key03',
+  e: 'key05',
+  r: 'key06',
+  t: 'key08',
+  y: 'key10',
+  u: 'key12',
+  v: 'key13',
+  b: 'key15',
+  n: 'key17',
+  m: 'key18',
+  ',': 'key20',
+  '.': 'key22',
+  '/': 'key24',
+  2: 'key02',
+  3: 'key04',
+  5: 'key07',
+  6: 'key09',
+  7: 'key11',
+  g: 'key14',
+  h: 'key16',
+  k: 'key19',
+  l: 'key21',
+  ';': 'key23',
+};
+
+document.addEventListener('keydown', (event) => {
+  const note = keyMap[event.key];
+  if (note) {
+    const keyElement = document.querySelector(`[data-key="${note}"]`);
+    if (keyElement) {
+      keyElement.click();
+    }
+  }
+});
+
 document.onload = selectDifficulty();
