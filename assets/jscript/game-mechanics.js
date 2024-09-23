b3ff4b05462ab32a235feac2eb3bdd3e3bacf110
Damir Ćorić <cordamir@gmail.com>
2024-09-23 13:20:50 +0000

    help me

--- assets/jscript/game-mechanics.js
+++ assets/jscript/game-mechanics.js
@@ -140,13 +140,13 @@ function drawSong(){
 }
 
 function endGame(){
-    alert(document.querySelector("final-screen-max"));
+    document.getElementsByClassName("game-end-container")[0].classList.remove("hidden");
     document.getElementById("piano-container").classList.add("hide");
     document.getElementById("notedrop-canvas").style.display = "none";
     document.getElementById("score-display").classList.add("hide");
-    document.getElementsByClassName("game-end-container")[0].classList.remove("hidden");
-    document.querySelector("final-screen-max").innerText = toString(currentSongNotes.length);
-    document.querySelector("final-screen-score").innerText = toString(userScore);
+    alert(document.getElementById("final-screen-score"));
+    document.getElementById("final-screen-max").innerText = toString(currentSongNotes.length);
+    document.getElementById("final-screen-score").innerText = toString(userScore);
 }
 
 function enableAutoplay(){
